86cb7d14672e672d334b8c4ca424374eaa9475bc
guotf925 <guotf925@163.com>
2018-09-23 14:24:57 +0800

    test

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("hi world!");
+        System.out.println("hi world!!");
     }
 }
